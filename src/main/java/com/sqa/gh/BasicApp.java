b9bc25d28bb2c6a60ea18d18ac8059b42fc6afba
Jean-francois Nepton <sqasolution@hotmail.com>
2017-08-19 11:35:09 -0700

    Create Project Structure

---
+++ src/main/java/com/sqa/gh/BasicApp.java
@@ -0,0 +1,11 @@
+package com.sqa.gh;
+
+public class BasicApp {
+
+	public static void main(String[] args) {
+		System.out.print("┏(-_-)┛");
+		System.out.print("┗(-_- )┓");
+		System.out.print("┗(-_-)┛");
+		System.out.print("┏(-_-)┓");
+	}
+}
